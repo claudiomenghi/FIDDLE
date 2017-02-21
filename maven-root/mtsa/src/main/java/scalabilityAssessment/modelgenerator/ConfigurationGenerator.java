3a20db8824cff3534e61de3c2a200840a24f21c8
claudiomenghi <cla.menghi@gmail.com>
2017-02-21 09:18:57 +0100

    updates the case study

--- maven-root/mtsa/src/main/java/scalabilityAssessment/modelgenerator/ConfigurationGenerator.java
+++ maven-root/mtsa/src/main/java/scalabilityAssessment/modelgenerator/ConfigurationGenerator.java
@@ -4,37 +4,37 @@ import java.util.Iterator;
 
 public class ConfigurationGenerator implements Iterator<ModelConfiguration> {
 
-	private final int[] statesEnvironment={10, 100, 1000, 10000};
-//	private final int initStatesEnvironment = 10;
-//	private final int incrementStatesEnvironment = 10;
-//	private final int finalStatesEnvironment = 1000;
+	private final int[] statesEnvironment = { 10, 100, 1000};
+	// private final int initStatesEnvironment = 10;
+	// private final int incrementStatesEnvironment = 10;
+	// private final int finalStatesEnvironment = 1000;
 
-//	private final int initStatesController = 50;
-//	private final int incrementalStatesController = 50;
-//	private final int finalStatesController = 300;
+	// private final int initStatesController = 50;
+	// private final int incrementalStatesController = 50;
+	// private final int finalStatesController = 300;
 
-	private final int[] statesController={10, 50, 100, 250, 500, 750, 1000};
-//	private final int initStatesController = 200;
-//	private final int incrementalStatesController = 200;
-//	private final int finalStatesController = 1000;
-	
-//	private final int environmentTransitionRatio = 15;
-//	private final int controllerTransitionRatio = 22;
+	private final int[] statesController = { 10, 50, 100, 250, 500, 750, 1000 };
+	// private final int initStatesController = 200;
+	// private final int incrementalStatesController = 200;
+	// private final int finalStatesController = 1000;
+
+	// private final int environmentTransitionRatio = 15;
+	// private final int controllerTransitionRatio = 22;
 
 	private final int environmentTransitionRatio = 10;
 	private final int controllerTransitionRatio = 10;
 
-
-	private int indexCurrentStateEnvironment=0;
-	private int indexCurrentStateController=0;
+	private int indexCurrentStateEnvironment = 0;
+	private int indexCurrentStateController = 0;
 	private int currentStatesEnvironment = statesEnvironment[0];
 	private int currentStatesController = statesController[0];
-	
-	private boolean first = true;
+
+	boolean first = true;
 
 	@Override
 	public boolean hasNext() {
-		if (indexCurrentStateEnvironment == statesEnvironment.length-1 && indexCurrentStateController == statesController.length-1) {
+		if (indexCurrentStateEnvironment == statesEnvironment.length - 1
+				&& indexCurrentStateController == statesController.length - 1) {
 			return false;
 		}
 		return true;
@@ -42,28 +42,35 @@ public class ConfigurationGenerator implements Iterator<ModelConfiguration> {
 
 	@Override
 	public ModelConfiguration next() {
-		if (indexCurrentStateEnvironment > statesEnvironment.length) {
-			throw new IllegalArgumentException("There is no next");
-		}
-			if (this.indexCurrentStateController < statesController.length) {
-				this.currentStatesController = this.statesController[this.indexCurrentStateController];
+		if (first) {
+			first = false;
+		} else {
+			if (indexCurrentStateEnvironment > statesEnvironment.length) {
+				throw new IllegalArgumentException("There is no next");
+			}
+			if (this.indexCurrentStateController < statesController.length-1) {
 				this.indexCurrentStateController++;
+				this.currentStatesController = this.statesController[this.indexCurrentStateController];
+				this.currentStatesEnvironment = this.statesEnvironment[this.indexCurrentStateEnvironment];
+
+				
 			} else {
 				this.indexCurrentStateEnvironment++;
-				this.currentStatesEnvironment = this.statesEnvironment[this.indexCurrentStateEnvironment] ;
-				this.indexCurrentStateController=0;
+				this.indexCurrentStateController = 0;
+				this.currentStatesController=this.statesController[0];
+				this.currentStatesEnvironment = this.statesEnvironment[this.indexCurrentStateEnvironment];
+				
 			}
-		
-		int transitionsEnvironment = this.currentStatesEnvironment
-				* this.environmentTransitionRatio;
+		}
+
+		int transitionsEnvironment = this.currentStatesEnvironment * this.environmentTransitionRatio;
 		// int eventsEnvironment = (int) Math
 		// .round(110.0 * this.currentStatesEnvironment / 10000);
-		int eventsEnvironment = this.statesController[2]*2;
+		int eventsEnvironment = this.statesController[1];
 		int transitionsController = this.currentStatesController * this.controllerTransitionRatio;
 		int eventsComponent = eventsEnvironment;
-				
 
-		int eventsComponentInterface = (int) Math.round(eventsComponent/100*25);
+		int eventsComponentInterface = (int) Math.round(eventsComponent / 100.0 * 25);
 		return new ModelConfiguration(this.currentStatesEnvironment, transitionsEnvironment, eventsEnvironment,
 				currentStatesController, transitionsController, eventsComponent, eventsComponentInterface);
 	}
--- maven-root/mtsa/src/main/java/scalabilityAssessment/modelgenerator/SubcontrollerGenerator.java
+++ maven-root/mtsa/src/main/java/scalabilityAssessment/modelgenerator/SubcontrollerGenerator.java
@@ -18,75 +18,63 @@ public class SubcontrollerGenerator {
 		this.numberOfStates = numberOfStates;
 	}
 
-	public LabelledTransitionSystem subController(
-			LabelledTransitionSystem controller) {
+	public LabelledTransitionSystem subController(LabelledTransitionSystem controller) {
 
-		LabelledTransitionSystem subcontroller = new LabelledTransitionSystem(
-				"SUB-CONTROLLER");
+		LabelledTransitionSystem subcontroller = new LabelledTransitionSystem("SUB-CONTROLLER");
 
 		subcontroller.setStates(new LTSTransitionList[numberOfStates + 1]);
 
-		int endStateIndex = numberOfStates ;
+		int endStateIndex = numberOfStates;
 
-		int[] newIndex=new int[controller.getAlphabet().length];
+		int[] newIndex = new int[controller.getAlphabet().length];
 		Arrays.fill(newIndex, -1);
-		
-		int currentFreeIndex=0;
+
+		int currentFreeIndex = 0;
 		// adding transitions between internal states
 		for (int stateIndex = 0; stateIndex < numberOfStates; stateIndex++) {
-			LTSTransitionList transitions = controller
-					.getTransitions(stateIndex);
-			Enumeration<LTSTransitionList> transitionList = transitions
-					.elements();
+			LTSTransitionList transitions = controller.getTransitions(stateIndex);
+			Enumeration<LTSTransitionList> transitionList = transitions.elements();
 			while (transitionList.hasMoreElements()) {
-				LTSTransitionList currentTransition = transitionList
-						.nextElement();
+				LTSTransitionList currentTransition = transitionList.nextElement();
 				if (currentTransition.getNext() < numberOfStates) {
-					
-					if(newIndex[currentTransition.getEvent()]==-1){
-						newIndex[currentTransition.getEvent()]=currentFreeIndex;
+
+					if (newIndex[currentTransition.getEvent()] == -1) {
+						newIndex[currentTransition.getEvent()] = currentFreeIndex;
 						currentFreeIndex++;
 					}
-					subcontroller.setState(stateIndex, LTSTransitionList
-							.addTransition(
-									subcontroller.getTransitions(stateIndex),
-									newIndex[currentTransition.getEvent()],
-									currentTransition.getNext()));
+					subcontroller.setState(stateIndex,
+							LTSTransitionList.addTransition(subcontroller.getTransitions(stateIndex),
+									newIndex[currentTransition.getEvent()], currentTransition.getNext()));
 				}
 			}
 		}
 		// initializing The alphabet
-		String[] alphabet=new String[currentFreeIndex];
-		for(int i=0; i<newIndex.length; i++){
-			if(newIndex[i]!=-1){
-				alphabet[newIndex[i]]=controller.getAlphabet()[i];
+		String[] alphabet = new String[currentFreeIndex];
+		for (int i = 0; i < newIndex.length; i++) {
+			if (newIndex[i] != -1) {
+				alphabet[newIndex[i]] = controller.getAlphabet()[i];
 			}
 		}
 		subcontroller.setAlphabet(alphabet);
-		
+
 		subcontroller.addFinalStateIndex(endStateIndex);
-		int endEventIndex = subcontroller.addEvent(LTLf2LTS.endSymbol
-				.getValue());
-		subcontroller
-				.addTransition(endStateIndex, endEventIndex, endStateIndex);
+		int endEventIndex = subcontroller.addEvent(LTLf2LTS.endSymbol.getValue());
+		subcontroller.addTransition(endStateIndex, endEventIndex, endStateIndex);
 		// adding transitions to final states
 		for (int stateIndex = 0; stateIndex < numberOfStates; stateIndex++) {
 			boolean found = false;
 
-			LTSTransitionList transitions = subcontroller
-					.getTransitions(stateIndex);
-			Enumeration<LTSTransitionList> transitionList = transitions
-					.elements();
-			while (transitionList.hasMoreElements() && !found) {
-				LTSTransitionList currentTransition = transitionList
-						.nextElement();
-				if (currentTransition.getNext() >= numberOfStates) {
-					found = true;
-					subcontroller
-							.setState(endStateIndex, LTSTransitionList
-									.addTransition(subcontroller
-											.getTransitions(endStateIndex),
-											endEventIndex, endStateIndex));
+			LTSTransitionList transitions = subcontroller.getTransitions(stateIndex);
+
+			if (transitions != null) {
+				Enumeration<LTSTransitionList> transitionList = transitions.elements();
+				while (transitionList.hasMoreElements() && !found) {
+					LTSTransitionList currentTransition = transitionList.nextElement();
+					if (currentTransition.getNext() >= numberOfStates) {
+						found = true;
+						subcontroller.setState(endStateIndex, LTSTransitionList.addTransition(
+								subcontroller.getTransitions(endStateIndex), endEventIndex, endStateIndex));
+					}
 				}
 			}
 		}
